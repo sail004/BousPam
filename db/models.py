bbf33fd79591ec9fc0137593be502b83df45e89a
Reidego <yep30042003@gmail.com>
2025-04-18 22:47:09 +0500

    fix: Changed models imports

--- db/models.py
+++ db/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Float, DateTime
 from sqlalchemy.dialects.postgresql import ARRAY
-from database import Base
+from db.database import Base
 
 
 class User(Base):
