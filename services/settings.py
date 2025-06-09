b8de632d556413bb7310a34aef2b40cc324586f1
Reidego <yep30042003@gmail.com>
2025-06-09 18:13:12 +0500

    feat: changed algorithm of generating jwt, hided in env

--- services/settings.py
+++ services/settings.py
@@ -1,10 +1,11 @@
-SECRET_KEY='gV64m9aIzFG4qpgVphvQbPQrtAO0nM-7YwwOvu0XPt5KJOjAy4AfgLkqJXYEt'
-ALGORITHM='HS256'
+import os
+from dotenv import load_dotenv
 
 
+load_dotenv()
 
-
-
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = os.getenv('ALGORITHM')
 
 def get_auth_data():
     return {"secret_key": SECRET_KEY, "algorithm": ALGORITHM}
